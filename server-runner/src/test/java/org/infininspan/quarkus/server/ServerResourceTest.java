09bae6dee5205520dec4b230a28a1f6394e470e4
Ryan Emerson <remerson@redhat.com>
2020-05-13 11:52:28 +0100

    #3 Reproducer

--- server-runner/src/test/java/org/infininspan/quarkus/server/ServerResourceTest.java
+++ server-runner/src/test/java/org/infininspan/quarkus/server/ServerResourceTest.java
@@ -1,12 +1,17 @@
 package org.infininspan.quarkus.server;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.commons.configuration.XMLStringConfiguration;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTestResource(ServerResourceTestResource.class)
 @QuarkusTest
@@ -27,5 +32,58 @@ public class ServerResourceTest {
             .then()
                .body(is("value-187"))
                .statusCode(200);
+   }
+
+   @Test
+   public void testGetProtobuf() {
+      given()
+            .when()
+            .get("/rest/v2/caches/___protobuf_metadata?action=size")
+            .then()
+            .body(is("0"))
+            .statusCode(200);
+   }
+
+   @Test
+   public void testHotRodGetCacheNames() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.addServer().host("127.0.0.1").port(11222);
+
+      RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
+      remoteCacheManager.getCacheNames();
+   }
+
+   @Test
+   public void testHotRodGetCache() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.addServer().host("127.0.0.1").port(11222);
+
+      RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
+      assertNotNull(remoteCacheManager.getCache("quarkus-infinispan-server"));
+   }
+
+   @Test
+   public void testHotRodInternalCacheOperations() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.addServer().host("127.0.0.1").port(11222);
+
+      RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
+      RemoteCache<Object, Object> cache = remoteCacheManager.getCache("___protobuf_metadata");
+      assertNotNull(cache);
+      cache.size();
+   }
+
+   @Test
+   public void testHotRodCreateCache() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.addServer().host("127.0.0.1").port(11222);
+
+      RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
+      XMLStringConfiguration config = new XMLStringConfiguration("<infinispan><cache-container><distributed-cache name=\"default\"/></cache-container></infinispan>");
+      RemoteCache<Object, Object> cache = remoteCacheManager
+            .administration()
+            .getOrCreateCache("created-cache", config);
+      assertNotNull(cache);
+      assertNotNull(remoteCacheManager.getCache("created-cache"));
    }
 }
