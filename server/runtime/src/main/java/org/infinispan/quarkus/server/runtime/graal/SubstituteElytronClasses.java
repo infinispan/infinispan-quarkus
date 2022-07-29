3c0f7b74d0636dd2cce7d21771c621303cf19995
Ryan Emerson <remerson@redhat.com>
2022-07-29 11:11:18 +0100

    Fixes #90 Remove key clone workarounds

--- server/runtime/src/main/java/org/infinispan/quarkus/server/runtime/graal/SubstituteElytronClasses.java
+++ server/runtime/src/main/java/org/infinispan/quarkus/server/runtime/graal/SubstituteElytronClasses.java
@@ -1,14 +1,6 @@
 package org.infinispan.quarkus.server.runtime.graal;
 
-import static java.security.AccessController.doPrivileged;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.lang.reflect.UndeclaredThrowableException;
-import java.security.Key;
-import java.security.PrivilegedAction;
 import java.util.Properties;
-import java.util.function.UnaryOperator;
 
 import org.infinispan.server.configuration.security.LdapRealmConfiguration;
 import org.infinispan.server.configuration.security.RealmConfiguration;
@@ -28,72 +20,5 @@ final class Target_LdapRealmConfiguration {
    @Substitute
    public SecurityRealm build(SecurityConfiguration security, RealmConfiguration realm, SecurityDomain.Builder domainBuilder, Properties properties) {
       return LdapSecurityRealmBuilder.builder().build();
-   }
-}
-
-@TargetClass(className = "org.wildfly.security.key.KeyUtil$KeyClonerCreator")
-final class Target_org_wildfly_security_key_KeyUtil_KeyClonerCreator {
-   @Substitute
-   private UnaryOperator<Key> checkForCloneMethod(final Class<?> declType, final Class<?> returnType) {
-      final Method method = doPrivileged(new PrivilegedAction<Method>() {
-         @Override
-         public Method run() {
-            try {
-               Method cloneMethod = declType.getDeclaredMethod("clone");
-               if (cloneMethod.getReturnType() == returnType)
-                  return cloneMethod;
-
-               return null;
-            } catch (NoSuchMethodException e) {
-               return null;
-            }
-         }
-      });
-
-      if (method == null)
-         return null;
-
-      return new UnaryOperator<Key>() {
-         @Override
-         public Key apply(Key key) {
-            try {
-               return (Key) method.invoke(key);
-            } catch (RuntimeException | Error e) {
-               throw e;
-            } catch (Throwable throwable) {
-               throw new UndeclaredThrowableException(throwable);
-            }
-         }
-      };
-   }
-
-   @Substitute
-   private UnaryOperator<Key> checkForCopyCtor(final Class<?> declType, final Class<?> paramType) {
-      final Constructor<?> constructor = doPrivileged(new PrivilegedAction<Constructor<?>>() {
-         @Override
-         public Constructor<?> run() {
-            try {
-               return declType.getDeclaredConstructor(paramType);
-            } catch (NoSuchMethodException e) {
-               return null;
-            }
-         }
-      });
-
-      if (constructor == null)
-         return null;
-
-      return new UnaryOperator<Key>() {
-         @Override
-         public Key apply(Key key) {
-            try {
-               return (Key) constructor.newInstance(key);
-            } catch (RuntimeException | Error e) {
-               throw e;
-            } catch (Throwable throwable) {
-               throw new UndeclaredThrowableException(throwable);
-            }
-         }
-      };
    }
 }
